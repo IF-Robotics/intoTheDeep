06300801858d872305a2bad9a9ce6112bbe40eac
Jicah0 <109925131+Jicah0@users.noreply.github.com>
2024-10-12 19:19:01 -0700

    more refactoring missed in last commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveSubsystem.java
@@ -1,2 +1,4 @@
-package org.firstinspires.ftc.teamcode;public class driveSubsystem {
+package org.firstinspires.ftc.teamcode;
+
+public class driveSubsystem {
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/runner/Drawing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/runner/Drawing.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.runner;
 
 import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.roadrunner.Pose2d;
