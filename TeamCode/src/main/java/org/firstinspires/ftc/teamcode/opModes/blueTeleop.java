4c5c80275b42edf3b2b08e596654bbc908b86b72
Jicah0 <109925131+Jicah0@users.noreply.github.com>
2025-01-15 00:57:48 -0800

    comment out references to vision subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/blueTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/blueTeleop.java
@@ -1,19 +1,19 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
-import static org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem.alliance;
+//import static org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem.alliance;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.commands.VisionClawCommand;
+//import org.firstinspires.ftc.teamcode.commands.VisionClawCommand;
 import org.firstinspires.ftc.teamcode.other.Robot;
 import org.firstinspires.ftc.teamcode.opModes.TeleopOpMode;
-import org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem;
+//import org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem;
 @TeleOp
 public class blueTeleop extends TeleopOpMode {
     public void initialize(){
         super.initialize();
-        visionSubsystem.setAlliance(VisionSubsystem.Alliance.RED); //They're opposites
-        alliance = VisionSubsystem.Alliance.RED;
+//        visionSubsystem.setAlliance(VisionSubsystem.Alliance.RED); //They're opposites
+//        alliance = VisionSubsystem.Alliance.RED;
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/redTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/redTeleop.java
@@ -1,19 +1,19 @@
 package org.firstinspires.ftc.teamcode.opModes;
 
-import static org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem.alliance;
+//import static org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem.alliance;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.commands.VisionClawCommand;
+//import org.firstinspires.ftc.teamcode.commands.VisionClawCommand;
 import org.firstinspires.ftc.teamcode.other.Robot;
 import org.firstinspires.ftc.teamcode.opModes.TeleopOpMode;
-import org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem;
+//import org.firstinspires.ftc.teamcode.subSystems.VisionSubsystem;
 @TeleOp
 public class redTeleop extends TeleopOpMode {
     public void initialize(){
         super.initialize();
-        visionSubsystem.setAlliance(VisionSubsystem.Alliance.BLUE); //They're opposites
-        alliance = VisionSubsystem.Alliance.BLUE;
+//        visionSubsystem.setAlliance(VisionSubsystem.Alliance.BLUE); //They're opposites
+//        alliance = VisionSubsystem.Alliance.BLUE;
     }
 
 
